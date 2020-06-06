9d2794c665254b341b057f4485aab0bede1d9f9f
Felix Andrew Sapalaran <40646937+felixandrewsapalaran@users.noreply.github.com>
2020-06-06 17:05:26 -0400

    Update __init__.py

--- phrase-hunter/phrasehunter/__init__.py
+++ phrase-hunter/phrasehunter/__init__.py
@@ -1,3 +1,10 @@
+"""
+Python Development Techdegree
+Project 3 - Phrase Hunters
+Author: Felix Andrew Sapalaran (felixandrewsapalaran@gmail.com)
+---------------------------------------------------------------
+"""
+
 # The following means that Game is accessible from
 # the phrasehunter module namespace directly (without needing to
 # specify the game submodule)
